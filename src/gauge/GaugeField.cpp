75e0760573482f95f86bc1f8536776962fc769a5
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-05-27 16:46:49 +0200

    Modify compute_staple for coeffs

--- src/gauge/GaugeField.cpp
+++ src/gauge/GaugeField.cpp
@@ -74,25 +74,23 @@ void GaugeField::project_field_su3() {
     }
 }
 
-// Computes the sum of all staples of a site
+// Computes the sum of all staples of a site with correct OBC coeffs
 void GaugeField::compute_staple(const Geometry& geo, size_t site, int mu, SU3& staple) const {
     staple.setZero();
     for (int nu = 0; nu < 4; nu++) {
         if (nu == mu) {
             continue;
         }
-        size_t x = site;                              // x
-        size_t xmu = geo.get_neigh(x, mu, up);        // x+mu
-        size_t xnu = geo.get_neigh(x, nu, up);        // x+nu
-        size_t xmunu = geo.get_neigh(xmu, nu, down);  // x+mu-nu
-        size_t xmnu = geo.get_neigh(x, nu, down);     // x-nu
-        auto U0 = view_link_const(xmu, nu);
-        auto U1 = view_link_const(xnu, mu);
-        auto U2 = view_link_const(x, nu);
-        staple += U0 * U1.adjoint() * U2.adjoint();
-        auto V0 = view_link_const(xmunu, nu);
-        auto V1 = view_link_const(xmnu, mu);
-        auto V2 = view_link_const(xmnu, nu);
-        staple += V0.adjoint() * V1.adjoint() * V2;
+        for (int j = 0; j<6; j++){
+            if (geo.is_staple_valid(site, mu, j)){
+                auto link_0 = geo.get_link_staple(site, mu, j, 0);
+                auto link_1 = geo.get_link_staple(site, mu, j, 1);
+                auto link_2 = geo.get_link_staple(site, mu, j, 2);
+                auto U0 = view_link_const(link_0.first, link_0.second);
+                auto U1 = view_link_const(link_1.first, link_1.second);
+                auto U2 = view_link_const(link_2.first, link_2.second);
+                staple += geo.get_staple_coeff(site, mu, j) * U0 * U1 * U2;
+            }
+        }
     }
 }
